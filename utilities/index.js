eb8b275e68bf3d7ff6cc94c1a0431777d6a8a564
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-03-07 16:41:23 -0500

    Week 3, Learning Activity 1.1, MVC Getting Started
    https://blainerobertson.github.io/340-js/views/mvc-start.html

---
+++ utilities/index.js
@@ -0,0 +1,23 @@
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* ************************
+ * Constructs the nav HTML unordered list
+ ************************** */
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications()
+  let list = '<a href="/" title="Home page">Home</a></li>'
+  data.rows.forEach((row) => {
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>"
+  })
+  return list
+}
+
+module.exports = Util
