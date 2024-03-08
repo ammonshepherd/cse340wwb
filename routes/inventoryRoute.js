b381eccb72bc370d352b408abb63a3d07b813eb9
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-03-07 21:48:01 -0500

    Week 3, Learning Activity 1.3, Build inventory route controller and model
    https://blainerobertson.github.io/340-js/views/inv-delivery-classification.html
    
    This fixes the route, removing the utility that we haven't build yet at this point.

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,7 +6,7 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get(
   "/type/:classificationId",
-  utilities.handleErrors(invController.buildByClassificationId)
+  invController.buildByClassificationId
 )
 
 module.exports = router
